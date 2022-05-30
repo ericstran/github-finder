f39d415ac027b1ae6f3b2f5b353cfb42d16678b9
Eric Stranquist <ericstran.stl@gmail.com>
2022-05-30 14:43:35 -0500

    querries 30 users from github api

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import UserResults from '../components/users/UserResults'
+
+const Home = () => {
+	return (
+		<>
+			{/* Search Component here*/}
+			<UserResults />
+		</>
+	)
+}
+
+export default Home
